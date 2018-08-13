51833424289f0482e4ebee033722713555c037dd
shamith16 <shamith@firehound.org>
2018-08-13 06:04:53 +0000

    Initial modification for Los
    Signed-off-by: shamith16 <shamith@firehound.org>

--- lineage_kenzo.mk
+++ lineage_kenzo.mk
@@ -17,14 +17,14 @@
 
 $(call inherit-product, device/xiaomi/kenzo/full_kenzo.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/fh/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := kenzo
-PRODUCT_NAME := fh_kenzo
+PRODUCT_NAME := lineage_kenzo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 3
 TARGET_VENDOR := Xiaomi
